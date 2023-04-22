896466dc6d55f7d58ccdbb6b67650c3a472ed75c
eRojasMarifil <e.rojas.marifil@outlook.cl>
2023-04-22 14:01:19 -0400

    creando repositorio, cargando s3

---
+++ S3/CODIGOS/Cue-EvelynRojas.py
@@ -0,0 +1,9 @@
+#EJERCICIO:
+#Requerimos imprimir en pantalla el resultado de la multiplicación de dos variables, las cuales
+#llamaremos “a y b”, y les asignaremos los valores 20 y 30, respectivamente. Debemos documentar 
+#cada paso de la operación haciendo uso de comentarios.
+
+a=20 #Definición de variable y valor a
+b=30 #Definición de variable y valor b
+
+print(a*b) #realizando multiplicación de a y b e imprimiendo
---
+++ S3/CODIGOS/Drilling-EvelynRojas.py
@@ -0,0 +1,14 @@
+#EJERCICIO:
+#El objetivo de la presente actividad es realizar un cálculo aritmético sencillo, haciendo uso de 
+#variables, operaciones aritméticas, y de la función print(). Para ello requerimos importar el 
+#módulo math.
+#ENUNCIADO DEL PROBLEMA:
+#Requerimos hacer el cálculo de la raíz cuadrada de una variable llamada “y” que tiene valor 81, 
+#utilizando el módulo de la librería math incorporada en Python. El resultado debemos asignarlo a 
+#una variable que será impresa en pantalla.
+
+import math #se importa la librería
+
+y=81 #Se define la variable y su valor
+
+print(int (math.sqrt(y))) #Se calcula la raíz con la función de la librería, se pasa a entero (porque no me gusta en flotante) y se imprime.
